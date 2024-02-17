6133359d8d98e06cd16f4b87b0989e4b18a860ee
JeevanRajpurohit <140495760+JeevanRajpurohit@users.noreply.github.com>
2024-02-17 16:40:59 +0530

    adding direction of snake +random food

--- snake.js
+++ snake.js
@@ -2,10 +2,9 @@ var canvas, pen, cs, snake;
 
 function init() {
     canvas = document.getElementById("myCanvas");
-    H = W = canvas.width = canvas.height = 1000;
+    canvas.width = canvas.height = 500; // Setting canvas dimensions directly
     pen = canvas.getContext('2d');
     cs = 66; // Reduced cell size to 10 pixels
-  
 
     snake = {
         init_len: 5,
@@ -24,28 +23,80 @@ function init() {
                 pen.fillRect(this.cells[i].x * cs, this.cells[i].y * cs, cs - 2, cs - 2);
             }
         },
-        updateSnake:function(){
-            console.log("Updating snake ...");
-            this.cells.pop();
-            var headX=this.cells[0].x;
-            var headY=this.cells[0].y;
+        updateSnake: function() {
+            console.log("Updating snake according to direction of snake...");
+            ////we have to check if the snake has eaten the food,
+            // and if eaten then we have to increase the size of the snake.
+            
 
-            var X=headX+1;
-            var Y=headY;
-            this.cells.unshift({x:X,y:Y});
+           
+            var headX = this.cells[0].x;
+            var headY = this.cells[0].y;
 
+
+            if(headX==food.x && headY==food.y){
+                console.log("food eaten");
+                food=getRamdomFood();
+            }else{
+                this.cells.pop();
+            }
+            var nextX, nextY;
+
+            if (this.direction == "right") {
+                nextX = headX + 1;
+                nextY = headY;
+            } else if (this.direction == "left") {
+                nextX = headX - 1;
+                nextY = headY;
+            } else if (this.direction == "down") {
+                nextX = headX;
+                nextY = headY + 1;
+            } else {
+                nextX = headX;
+                nextY = headY - 1;
+            }
+            this.cells.unshift({ x: nextX, y: nextY });
         }
     };
     snake.createSnake();
+
+    // Add an event listener to handle key presses
+    function keyPressed(e) {
+        if (e.key == "ArrowRight") {
+            snake.direction = "right";
+        } else if (e.key == "ArrowLeft") {
+            snake.direction = "left";
+        } else if (e.key == "ArrowDown") {
+            snake.direction = "down";
+        } else if (e.key == "ArrowUp") { // Corrected the key for moving up
+            snake.direction = "up";
+        }
+        console.log(snake.direction);
+    }
+    document.addEventListener('keydown', keyPressed);
 }
 
 function draw() {
+    pen.clearRect(0, 0, W, H);
     snake.drawSnake();
+    pen.fillStyle=food.color;
+    pen.fillRect(food.x*cs,food.y*cs,cs,cs);
 }
 
 function update() {
-  //aold frame erasing pending .........
+    // Update the snake position
     snake.updateSnake();
+}
+function getRamdomFood{
+       var foodX=Math.round(Math.random()*(W-cs)/cs);
+       var foodY=Math.round(Math.random()*(H-cs)/cs);
+
+       var food={
+          x:foodX,
+          y:foodY,
+          color : "red",
+       }
+       return food;
 }
 
 function gameloop() {
